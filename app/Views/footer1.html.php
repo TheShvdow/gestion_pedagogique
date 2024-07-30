40a5192adebe4b064829577fe748937debd0e5b2
idrissa wade <derisswvde@gmail.com>
2024-07-30 14:21:04 +0000

    modified:   app/Views/footer1.html.php

--- app/Views/footer1.html.php
+++ app/Views/footer1.html.php
@@ -2,7 +2,7 @@
 
 <footer class="bg-white shadow-lg rounded-lg dark:bg-gray-900 m-4 absolute bottom-0 w-[83vw]">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <!-- <div class="sm:flex sm:items-center sm:justify-between">
+        <div class="sm:flex sm:items-center sm:justify-between">
             <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJd6peDvOqaNnWCcLH1ZEHmgIx2oCwVyBEkQ&s" class="h-8" alt="Ecole 221 Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ecole 221</span>
@@ -21,7 +21,7 @@
                     <a href="#" class="hover:underline">Contact</a>
                 </li>
             </ul>
-        </div> -->
+        </div>
     
         <span class="block textlg text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">Ecole 221</a>. All Rights Reserved.</span>
     </div>
